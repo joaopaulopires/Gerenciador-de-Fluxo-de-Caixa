59a6a6508a48f9fc94c0bdde86e01f2098bc511f
joaopaulopires <joaopaulopiresdev@gmail.com>
2021-10-24 07:10:11 -0300

    Sem input de dados no front-end

---
+++ gerenciador/views.py
@@ -0,0 +1,13 @@
+'''from django.http import HttpResponse
+from django.shortcuts import render
+from .models import ControleCaixa
+
+def login(request):
+    return HttpResponse('Área de Login')
+
+def controleCaixa(request):
+    return render(request, 'controle_caixa.html')
+
+def resumoFinanceiro(request):
+    return render(request, 'resumo_financeiro.html')'''
+
