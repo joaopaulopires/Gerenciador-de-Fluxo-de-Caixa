a1448ed127967e503abaddbcd63ddeec8f66a925
joaopaulopires <joaopaulopiresdev@gmail.com>
2021-11-24 16:43:03 -0300

    Configurando o Heroku

--- gerenciador/settings.py
+++ gerenciador/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from decouple import config
+from dj_database_url import parse as dburl
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)hy5rhf*kg=f5z0b56342xy!a%kf-tdpjg%ww%z+2rjh7_8gur'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gerenciador-caixa.herokuapp.com']
 
 
 # Application definition
@@ -74,16 +77,12 @@ WSGI_APPLICATION = 'gerenciador.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+default_dburl = 'sqlite: ///' + os.path.join (BASE_DIR, 'db.sqlite3')
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'gerenciador_caixa',
-        'USER': 'postgres',
-        'PASSWORD': 'cananeia1',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
+    'default': config ('DATABASE_URL', default = default_dburl, cast = dburl),
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -131,3 +130,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join (BASE_DIR, 'staticfiles')
